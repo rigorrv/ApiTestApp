b3cb2d488b9c1a55dd3e9a268f03a941a88cc123
Rigo Romero <Rigo.Romero@7-11.com>
2023-12-22 11:13:21 -0700

    compose steppers

--- app/src/main/java/com/example/apitestapp/api/Api.kt
+++ app/src/main/java/com/example/apitestapp/api/Api.kt
@@ -2,6 +2,7 @@ package com.example.apitestapp.api
 
 import com.example.apitestapp.model.MovieDB
 import com.example.apitestapp.utilities.ApplicationConstants.endPoint
+import com.example.apitestapp.utilities.ApplicationConstants.searchEndPoint
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -12,7 +13,7 @@ interface Api {
     @GET(endPoint)
     suspend fun getMovies(@Header("Authorization") value: String): Response<MovieDB>
 
-    @GET("/3/search/movie")
+    @GET(searchEndPoint)
     suspend fun searchMovie(
         @Query("query") search: String,
         @Header("Authorization") value: String
