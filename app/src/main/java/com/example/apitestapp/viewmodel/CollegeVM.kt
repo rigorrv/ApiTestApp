0e519301b84a094115906021fb2824ffd19f5642
rigo <rigo.rrv@gmail.com>
2024-02-13 20:35:19 -0700

    Photon Test

--- app/src/main/java/com/example/apitestapp/viewmodel/CollegeVM.kt
+++ app/src/main/java/com/example/apitestapp/viewmodel/CollegeVM.kt
@@ -4,11 +4,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.apitestapp.model.ContentDB
 import com.example.apitestapp.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class CollegeVM @Inject constructor(private val repository: Repository) : ViewModel() {
 
     private val _collegeStateFlow = MutableStateFlow<ContentDB?>(null)
